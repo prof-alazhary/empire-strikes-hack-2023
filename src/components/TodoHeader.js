5a81a288bdff56d68c366000392660ae59c49e23
Mohamed ElAzhary <mohamed.elazhary@vodafone.com>
2023-04-06 10:26:29 +0200

    init the integration with the ChatGPt API.

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,24 +1,48 @@
 import React from 'react';
-
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
 class TodoHeader extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.textInput = React.createRef();
     }
-    getRandmoId(){
+    getRandmoId() {
         //this is just for testing purposes
         //afterthat we'll get the Id from the DB..
         const id = Math.random().toString();
-        return id.slice(2,5);
+        return id.slice(2, 5);
     }
-    handleAddTodo(e){
+    handleAddTodo(e) {
         const txt = this.textInput.current;
-        if(txt.value){
-            const todo = { title: txt.value , checked: false, id: this.getRandmoId() }
+        const inputValue = txt.value;
+        if (inputValue) {
+            const todo = { title: inputValue, checked: false, id: this.getRandmoId() }
             this.props.onTodoAdd(todo);
             txt.value = "";
+            this.generateResponse(inputValue);
         }
     }
+    async generateResponse(prompt) {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                'prompt': prompt,
+                'max_tokens': 150,
+                'n': 1,
+                'stop': '\n',
+            }),
+        });
+        const responseJson = await response.json();
+        console.log("responseJson --->", responseJson)
+        const { choices } = (responseJson && responseJson.choices && responseJson.choices[0]) || {};
+        const answer = choices && choices[0] && choices[0].text;
+        return answer;
+    }
+
     render() {
         return (
             <div id="myDIV" className="header">
